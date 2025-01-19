136fc380a3ac3f80dfb14b8525344874d26ee6bc
Md. Nobir Hasan <nobir.wd@gmail.com>
2025-01-19 19:08:07 +0600

    log adding for csrf token mismatching

--- app/Http/Controllers/LoginStoreController.php
+++ app/Http/Controllers/LoginStoreController.php
@@ -5,11 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 
 class LoginStoreController extends Controller
 {
+    public function showLoginForm()
+    {
+        Log::info('Form Load Session:', [
+            'session_id' => session()->getId(),
+            'token' => csrf_token(),
+            'session_path' => config('session.files'),
+            'domain' => config('session.domain')
+        ]);
+
+        return view('auth.login');
+    }
     public function login(Request $request){
+        Log::info('Form Load Session:', [
+            'session_id' => session()->getId(),
+            'token' => csrf_token(),
+            'session_path' => config('session.files'),
+            'domain' => config('session.domain')
+        ]);
        $email = $request->email;
        $password = $request->password;
        $this->validate($request,[
