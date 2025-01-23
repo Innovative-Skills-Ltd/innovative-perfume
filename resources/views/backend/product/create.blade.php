f766ad2bafd079826cac468c21e26c077942f2bc
Md. Nobir Hasan <nobir.wd@gmail.com>
2025-01-23 16:10:56 +0600

    slug done

--- resources/views/backend/product/create.blade.php
+++ resources/views/backend/product/create.blade.php
@@ -6,6 +6,15 @@
     <div class="card">
         <h5 class="card-header">Add Product</h5>
         <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <form method="post" action="{{ route('product.store') }}" enctype="multipart/form-data">
                 {{ csrf_field() }}
                 <div>
