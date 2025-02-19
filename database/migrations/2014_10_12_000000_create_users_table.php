9e59475c645d88cc271984737ff47e6eb7380bcc
Md. Nobir Hasan <nobir.wd@gmail.com>
2025-02-19 19:03:02 +0600

    brand nullable, sub category nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -26,7 +26,6 @@ class CreateUsersTable extends Migration
             $table->integer('post_code')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            // $table->string('decrypt_password')->nullable();
             $table->string('photo')->nullable();
             $table->bigInteger('social_id')->nullable();
             $table->string('role')->default('user');
